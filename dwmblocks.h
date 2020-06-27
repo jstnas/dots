ed7e3a0f40c42880fb76684d89b49501ef127ba6
Justinas Grigas <me@jstnas.com>
2020-06-27 15:35:01 +0100

    Adding more status scripts.

--- dwmblocks.h
+++ dwmblocks.h
@@ -2,9 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
 
-	{"B:",	"~/bin/status/backlight",	0,	1},
-	{"B:",	"~/bin/status/battery",		1,	0},
-	{"T:",	"~/bin/status/clock",		1,	0},
+	{"",	"~/bin/status/temperature",	1,	0},
+	{"",	"~/bin/status/backlight",	0,	1},
+	{"",	"~/bin/status/battery",		1,	0},
+	{"",	"~/bin/status/clock",		1,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
