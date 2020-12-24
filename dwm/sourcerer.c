425672eea0ae5b7135a6ab6a82b204f6ca07a8a1
Justinas Grigas <jstn_as@protonmail.com>
2020-12-24 01:00:49 +0000

    Updated dwm palettes.

---
+++ dwm/sourcerer.c
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#111111";
+static const char col_gray2[] = "#181818";
+static const char col_gray3[] = "#d3d3d3";
+static const char col_gray4[] = "#c1cdc1";
+static const char col_cyan[]  = "#8181a6";
