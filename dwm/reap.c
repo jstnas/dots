00cb810321110218dba6eafa88d8f7875b37efc4
Justinas Grigas <jstn_as@protonmail.com>
2020-12-24 04:20:30 +0000

    Added the reap colours.

---
+++ dwm/reap.c
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#1a1a1a";
+static const char col_gray2[] = "#4d4d4d";
+static const char col_gray3[] = "#999999";
+static const char col_gray4[] = "#e6e6e6";
+static const char col_cyan[]  = "#4d6e80";
