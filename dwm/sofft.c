4b995a57e7cd4d1cd039c98e2df3447abee3f672
Justinas Grigas <jstn_as@protonmail.com>
2021-01-01 06:46:55 +0000

    Added sofft palette.

---
+++ dwm/sofft.c
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#0f0f0f";
+static const char col_gray2[] = "#3f3f3f";
+static const char col_gray3[] = "#cfcfcf";
+static const char col_gray4[] = "#ffffff";
+static const char col_cyan[]  = "#ff3f7f";
