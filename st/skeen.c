3ff0b450351204ea1c4f360475e281166bca1d12
Justinas Grigas <jstn_as@protonmail.com>
2021-01-18 16:18:15 +0000

    Added skeen palette

---
+++ st/skeen.c
@@ -0,0 +1,23 @@
+static const char *colorname[] = {
+	[0] = "#000000", // black
+	[1] = "#cb6072", // red
+	[2] = "#73a079", // green
+	[3] = "#efcf84", // yellow
+	[4] = "#426f90", // blue
+	[5] = "#856f95", // magenta
+	[6] = "#9dc3b4", // cyan
+	[7] = "#d8d2c6", // white
+	// bright
+	[8]  = "#3f3f3f", // black
+	[9]  = "#cb6072", // red
+	[10] = "#73a079", // green
+	[11] = "#efcf84", // yellow
+	[12] = "#426f90", // blue
+	[13] = "#856f95", // magenta
+	[14] = "#9dc3b4", // cyan
+	[15] = "#efeee9", // white
+	// extra
+	[256] = "#cb6072", // cursor 8
+	[257] = "#cb6072", // reverse cursor 7
+	[258] = "#000000", // background 0
+};
