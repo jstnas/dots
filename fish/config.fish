fedc88cc68d658ae2b0333aa580ec25e7d2aca64
Justinas Grigas <jstn_as@protonmail.com>
2021-03-10 11:03:59 +0000

    Tweaks - Changed dunst size - Configured fish colours - Added picom back to xinit - Added mac demarco to ytdl

--- fish/config.fish
+++ fish/config.fish
@@ -5,11 +5,32 @@ set fish_cursor_insert line
 set fish_cursor_replace_one line blink
 set fish_cursor_visual block
 
+## Colours.
+set fish_color_normal			white
+set fish_color_command			brgreen
+set fish_color_quote			yellow
+set fish_color_redirection		magenta
+set fish_color_end				white
+set fish_color_error			brred
+set fish_color_param			brcyan
+set fish_color_comment			white
+set fish_color_match			green
+set fish_color_selection		white --background=magenta
+set fish_color_search_match		white --background=brblack
+set fish_color_operator			brmagenta
+set fish_color_escape			yellow
+set fish_color_cwd				yellow
+set fish_color_autosuggestion	brblack
+set fish_color_user				green
+set fish_color_host				cyan
+set fish_color_host_remote		yellow
+set fish_color_cancel			brred
+
 ## Variables
-set -U EDITOR (which vim)
-set -U BROWSER (which surf)
-set -U GPG_TTY (tty)
-set -U PF_INFO 'ascii title os host kernel uptime pkgs memory palette'
+set -x EDITOR vim
+set -x BROWSER surf
+set -x GPG_TTY (tty)
+set -x PF_INFO 'ascii title os host kernel uptime pkgs memory palette'
 
 ## Abbreviations
 abbr -a cb 'cmake --build'
@@ -22,6 +43,7 @@ abbr -a pls 'pacman -Qq | grep -i'
 abbr -a Sxiv  'sxiv -o'
 abbr -a Picom 'picom --experimental-backends & disown'
 abbr -a vimrc 'vim ~/.vim/vimrc'
+abbr -a xrdbm 'xrdb -merge ~/.Xresources'
 # Git
 abbr -a ga 'git add'
 abbr -a gc 'git commit -S -m'
