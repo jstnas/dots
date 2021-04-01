51ccd4f2c3987af7903fcc8380be9ad1e8124986
Justinas Grigas <jstn_as@protonmail.com>
2021-04-01 05:10:10 +0100

    Fixed vim mode indicator contrast

--- fish/functions/fish_default_mode_prompt.fish
+++ fish/functions/fish_default_mode_prompt.fish
@@ -3,19 +3,19 @@ function fish_default_mode_prompt
 		or test "$fish_key_bindings" = fish_hybrid_key_bindings
 		switch $fish_bind_mode
 			case default
-				set_color --bold --background magenta black
+				set_color --bold --background magenta brwhite
 				echo ' N '
 			case insert
-				set_color --bold --background blue black
+				set_color --bold --background blue brwhite
 				echo ' I '
 			case replace_one
-				set_color --bold --background red black
+				set_color --bold --background red brwhite
 				echo ' r '
 			case replace
-				set_color --bold --background red black
+				set_color --bold --background red brwhite
 				echo ' R '
 			case visual
-				set_color --bold --background yellow black
+				set_color --bold --background yellow brwhite
 				echo ' V '
 		end
 		set_color normal
