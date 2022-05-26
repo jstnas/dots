525a45e1273b7dcd4317775dda1eb45468435c81
Justinas Grigas <jstn_as@protonmail.com>
2022-05-26 13:47:51 +0100

    fish: updated colors

--- fish/colors.fish
+++ fish/colors.fish
@@ -1,19 +1,31 @@
-set fish_color_normal			white
-set fish_color_command			green
-set fish_color_quote			yellow
-set fish_color_redirection		magenta
-set fish_color_end				white
-set fish_color_error			red
-set fish_color_param			cyan
-set fish_color_comment			white
-set fish_color_match			green
-set fish_color_selection		normal --background=brblack
-set fish_color_search_match		normal --background=brblack
-set fish_color_operator			magenta
-set fish_color_escape			yellow
-set fish_color_cwd				yellow
-set fish_color_autosuggestion	white
-set fish_color_user				green
-set fish_color_host				cyan
-set fish_color_host_remote		yellow
-set fish_color_cancel			brred
+# syntax
+set fish_color_normal						white
+set fish_color_command						yellow
+set fish_color_keyword						yellow
+set fish_color_quote						red
+set fish_color_redirection					magenta
+set fish_color_end							magenta
+set fish_color_error						red -r
+set fish_color_param						blue
+set fish_color_option						cyan
+set fish_color_comment						brblack
+set fish_color_selection					yellow -r
+set fish_color_operator						magenta
+set fish_color_escape						magenta
+set fish_color_autosuggestion				brblack
+set fish_color_cwd							yellow
+set fish_color_user							green
+set fish_color_host							cyan
+set fish_color_host_remote					yellow
+set fish_color_cancel						brred
+set fish_color_search_match					yellow
+# pager
+set fish_pager_color_progress				black -b green
+set fish_pager_color_background				-b normal
+set fish_pager_color_prefix					yellow
+set fish_pager_color_completion				white
+set fish_pager_color_description			cyan
+set fish_pager_color_selected_background	-b brblack
+set fish_pager_color_selected_prefix		bryellow
+set fish_pager_color_selected_completion	brwhite
+set fish_pager_color_selected_description	brcyan
